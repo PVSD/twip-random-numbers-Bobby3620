f2e0a2b02a49a7a1b56f6ee63baa177311721813
Bobby3620 <41356385+Bobby3620@users.noreply.github.com>
2018-11-28 20:17:05 -0500

    not done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+       randomNumber ranNum = new randomNumber();
+        ranNum.generateRanNum();
     }
 }
